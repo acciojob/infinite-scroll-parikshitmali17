5bc5d23d02572c558bb3f08d6fbcd57ec0d5e178
Parikshit Mali <parikshitmali17@gmail.com>
2025-02-22 20:40:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 //your code here!
+const body=document.getElementById("body")
+const infiniteList=document.getElementById("infi-list");
 
+let itemNo=11;
+
+function Infunction(e) {
+if (window.innerHeight + window.scrollY >= document.documentElement.scrollHeight - 10) {
+for (let i = 0; i < 2; i++){
+	let li1=document.createElement("li");
+	li.innerText=`Item ${itemNo}`
+	infiniteList.appendChild(li)
+itemNo++
+}
+}
+	
+}
+
+window.addEventListner("scroll",)
